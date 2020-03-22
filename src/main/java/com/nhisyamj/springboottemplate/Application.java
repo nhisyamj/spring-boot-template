695858f6e35bce9ed8fb7b640fbea6be74b0d11c
Hisyam Johan <hisyam@hitachi-ebworx.com>
2020-03-22 14:43:05 +0800

    enhance

--- src/main/java/com/nhisyamj/springboottemplate/Application.java
+++ src/main/java/com/nhisyamj/springboottemplate/Application.java
@@ -1,13 +1,25 @@
 package com.nhisyamj.springboottemplate;
 
+import com.nhisyamj.springboottemplate.constant.Constant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> factory.setContextPath(Constant.BASE_PATH);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		log.info("SwaggerUI now running on http://localhost:8080{}/swagger-ui.html", Constant.BASE_PATH);
 	}
 
 }
---
+++ src/main/java/com/nhisyamj/springboottemplate/constant/Constant.java
@@ -0,0 +1,5 @@
+package com.nhisyamj.springboottemplate.constant;
+
+public class Constant {
+    public static final String BASE_PATH = "/api/v1";
+}
